4ec6973acac12b3cef1821ff4b2aaa8371789a4a
choffice <choffice@naver.com>
2026-03-23 14:53:09 +0900

    css attribute till box (with punch)

---
+++ punch.js
@@ -0,0 +1,18 @@
+document.body.innerHTML += '<div id="vs" style="width:50vh;height:100vh;position:fixed;left:40.25vw;top:-20vh;"><div style="width:100%;height:100%;transform:rotateX(180deg);background:center/contain no-repeat url(https://cdn.pixabay.com/photo/2020/11/12/08/38/chicken-5734786_1280.png);"></div></div>';
+
+document.body.innerHTML += '<div id="punchL" style="width:20vw;height:20vw;position:fixed;bottom:0;left:20vw;"><div style="background:center/contain no-repeat url(https://cdn.pixabay.com/photo/2013/07/12/16/55/mitt-151498_1280.png);width:100%;height:100%;transform:rotate(-30deg);"></div></div>';
+
+document.body.innerHTML += '<div id="punchR" style="width:20vw;height:20vw;position:fixed;bottom:0;right:25vw;"><div style="background:center/contain no-repeat url(https://cdn.pixabay.com/photo/2013/07/12/16/55/mitt-151498_1280.png);width:100%;height:100%;transform:rotateY(180deg) rotate(-30deg);"></div></div>';
+let chick;
+document.onkeydown = (t_k) => {
+    clearTimeout(chick);
+    if (t_k.key == 'ArrowLeft') {
+        punchL.style.transform = 'translate(5vw,-5vw) scale(.8) rotateX(40deg)'; punchL.style.filter = 'drop-shadow(2em -2em 1em yellow)';
+        vs.classList = 'punch';
+    } else if (t_k.key == 'ArrowRight') {
+        punchR.style.transform = 'translate(-5vw,-5vw) scale(.8) rotateX(40deg)'; punchR.style.filter = 'drop-shadow(-2em -2em 1em red)';
+        vs.classList = 'punch';
+    }
+    chick = setTimeout(()=>{vs.removeAttribute('class');},500);
+}
+document.onkeyup = (t_k) => { t_k.key.substring(0,5) == 'Arrow' ? (punchL.style.transform = '', punchL.style.filter = '',punchR.style.transform = '', punchR.style.filter = '') : ''; }
--- style2/menu.js
+++ style2/menu.js
@@ -1,12 +1,12 @@
-let tmlist = '단위/글자속성'.split('/');
+let tmlist = 'css단위/글자속성/background/box속성'.split('/');
 
 if(location.href.includes('index.html')){
 for(a_tm of tmlist){
-homemenu.innerHTML+='<li><a href="'+(tmlist.indexOf(a_tm)+1<10?'0'+(tmlist.indexOf(a_tm)+1):(tmlist.indexOf(a_tm)+1))+'_css'+a_tm+'.html">css'+a_tm+'</a></li>'
+homemenu.innerHTML+='<li><a href="'+(tmlist.indexOf(a_tm)+1<10?'0'+(tmlist.indexOf(a_tm)+1):(tmlist.indexOf(a_tm)+1))+'_'+a_tm+'.html">'+a_tm+'</a></li>'
 }
 }else{
 document.body.innerHTML='<menu id="topmenu" style="display:flex;justify-content: end;list-style:none;align-items:center;font-size:1vw;"></menu>'+document.body.innerHTML;
 topmenu.innerHTML='<li style="margin-inline:2%;"><a href="index.html" style="text-decoration:none;"><p style="background:center/contain no-repeat url(https://cdn.pixabay.com/photo/2015/12/28/02/58/home-1110868_1280.png);color:transparent;padding:2%;">메인 홈으로</p></a></li>';
-for(a_tm of tmlist){topmenu.innerHTML+='<li style="margin-inline:2%;background:green;padding:1%;"><a href="'+(tmlist.indexOf(a_tm)+1<10?'0'+(tmlist.indexOf(a_tm)+1):(tmlist.indexOf(a_tm)+1))+'_css'+a_tm+'.html" style="text-decoration: none;color:#fff;">css'+a_tm+'</a></li>';}
+for(a_tm of tmlist){topmenu.innerHTML+='<li style="margin-inline:2%;background:green;padding:1%;"><a href="'+(tmlist.indexOf(a_tm)+1<10?'0'+(tmlist.indexOf(a_tm)+1):(tmlist.indexOf(a_tm)+1))+'_'+a_tm+'.html" style="text-decoration: none;color:#fff;">'+a_tm+'</a></li>';}
 
 document.querySelector('h1').style.marginTop = '10vh';}
